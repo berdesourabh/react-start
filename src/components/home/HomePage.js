0c66bd4c6ba9b3ad4952fe84c143056e680cfcd4
Sourabh Berde <sourabhberde@Sourabhs-MacBook-Air.local>
2016-12-24 16:34:41 +0530

    Initial commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello...!</h1>
+        <p>This is my first app</p>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ tools/startMessage.js
@@ -0,0 +1,5 @@
+import colors from 'colors';
+
+/*eslint-disable no-console */
+
+console.log('Starting app strats in dev mode ...'.green);
